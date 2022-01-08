7fd29826b48ced6f2c7f70a1f01ae24f6e3f2a54
The Casalegno <giuliocasalegno@libero.it>
2022-01-08 21:57:10 +0100

    Added LINKS.js

--- commands/links.js
+++ commands/links.js
@@ -15,25 +15,25 @@ module.exports = {
 				new Discord.MessageButton()
 					.setLabel('Invite')
 					.setStyle('LINK')
-                    .setEmoji('928710326105542706')
+                    .setEmoji('928710223701631046')
                     .setURL("https://discord.com/oauth2/authorize?client_id=925762773332418560&scope=bot+applications.commands&permissions=274878221376"),
                 new Discord.MessageButton()
 					.setLabel('Support')
 					.setStyle('LINK')
-                    .setEmoji('928710868974329866')
+                    .setEmoji('928708289578037298')
                     .setURL("https://discord.gg/gshJMkvRFZ"),
                 new Discord.MessageButton()
 					.setLabel('GitHub')
 					.setStyle('LINK')
-                    .setEmoji('928711456113959033')
+                    .setEmoji('928711386501103687')
                     .setURL("https://github.com/BotGround"),
 			);
         const embed = new Discord.MessageEmbed()
             .setAuthor({name: "Cypherer Help - All Links", iconURL: "https://iili.io/YMpe9f.md.png"})
-            .setDescription("These are the most important and useful links of the bot:\nINVITE ➜ [here](https://discord.com/oauth2/authorize?client_id=925762773332418560&scope=bot+applications.commands&permissions=274878221376)\nSUPPORT SERVER ➜ [join](https://discord.gg/gshJMkvRFZ)\nWEBSITE ➜ [here](https://botground.ga/cypherer)\nGITHUB ➜ [organization](https://github.com/BotGround)")
+            .setDescription("These are the most important and useful links of the bot:\n**INVITE** ➜ [here](https://discord.com/oauth2/authorize?client_id=925762773332418560&scope=bot+applications.commands&permissions=274878221376)\n**SUPPORT SERVER** ➜ [join](https://discord.gg/gshJMkvRFZ)\n**WEBSITE** ➜ [here](https://botground.ga/cypherer)\n**GITHUB** ➜ [organization](https://github.com/BotGround)")
             .setFooter(`Requested by ${user.tag}`, user.displayAvatarURL())
             .setTimestamp()
             .setColor("#4F646F")
-        interaction.reply({embeds: [embed], components: [row]})
+        interaction.reply({embeds: [embed], components: [row], ephemeral: true})
     }
 }
